34ca1e6aa83f22c774f7bdba87e7e59da49ebb70
zshaiyen <zaneshaiyen@gmail.com>
2026-05-25 23:20:13 -0700

    Fixed comma in stats.

--- site/assets/js/custom.js
+++ site/assets/js/custom.js
@@ -291,28 +291,26 @@
 
     }
 
-    $(window).scroll(function() {
+	$(window).scroll(function() {
 
-        if (visible($('.count-digit'))) {
-            if ($('.count-digit').hasClass('counter-loaded')) return;
-            $('.count-digit').addClass('counter-loaded');
+		if (visible($('.count-digit'))) {
+			if ($('.count-digit').hasClass('counter-loaded')) return;
+			$('.count-digit').addClass('counter-loaded');
 
-            $('.count-digit').each(function() {
-                var $this = $(this);
-                jQuery({
-                    Counter: 0
-                }).animate({
-                    Counter: $this.text()
-                }, {
-                    duration: 3000,
-                    easing: 'swing',
-                    step: function() {
-                        $this.text(Math.ceil(this.Counter));
-                    }
-                });
-            });
-        }
-    })
+			$('.count-digit').each(function() {
+				var $this = $(this);
 
-
+				jQuery({ Counter: 0 }).animate(
+					{ Counter: parseInt($this.text().replace(/,/g, '')) },
+					{
+						duration: 3000,
+						easing: 'swing',
+						step: function() {
+							$this.text(Math.ceil(this.Counter).toLocaleString());
+						}
+					}
+				);
+			});
+		}
+	});
 })(window.jQuery);
